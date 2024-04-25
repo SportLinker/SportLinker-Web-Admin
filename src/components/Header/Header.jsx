9e07ecf67c5fe7970e88d91adbb25d3f8446ae87
Phạm Bá Phước <phuoc.18112002@gmail.com>
2024-04-25 17:53:27 +0700

    setup project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100px",
+        padding: "0px 20px",
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "blueviolet",
+        alignItems: "center",
+        borderBottom: "1px solid #f5f5f5",
+      }}
+    >
+      Header
+    </div>
+  );
+};
---
+++ src/routes/AppRouters.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DefaultLayout } from "../layouts/DefaultLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { DashboardPage } from "../pages/DashboardPage/DashboardPage";
+import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
+import PermissionDeniedPage from "../pages/PermissionDeniedPage/PermissionDeniedPage";
+
+export const AppRouters = () => {
+  return (
+    <DefaultLayout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />}></Route>
+        <Route path="/dashboard" element={<DashboardPage />}></Route>
+        <Route
+          path="/permission-denied"
+          element={<PermissionDeniedPage />}
+        ></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </DefaultLayout>
+  );
+};
