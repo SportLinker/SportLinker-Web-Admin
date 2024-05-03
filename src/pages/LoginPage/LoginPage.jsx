1ed867e0970705a42c5fca81d9d20b17fa2b4d28
votantai3070 <votantai.07092002@gmail.com>
2024-05-03 12:16:35 +0700

    fix UI signup and login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,59 +1,80 @@
-import React from "react";
-import styles from "./Login.module.css"; // Import CSS module file for styling
-import { imageExporter } from "../../assets/images";
+import React from 'react';
+import {Form, Input, Button, Checkbox} from 'antd';
+import {imageExporter} from '../../assets/images';
+import styles from './Login.module.css'; // Import CSS module file for styling
 
 const LoginPage = () => {
-  return (
-    <div className={styles.loginPageContainer}>
-      <div className={styles.leftSide}>
-        <div className={styles.loginHeader}>
-          <h1>Sign In</h1>
-        </div>
-        <div className={styles.loginForm}>
-          <div className={styles.loginInput}>
-            <label>Email</label>
-            <input
-              type="text"
-              
-              className={styles.inputField}
-            />
-          </div>
-          <div className={styles.loginInput}>
-            <label>Password</label>
-            <input
-              type="password"
-              
-              className={styles.inputField}
-            />
-          </div>
+	const onFinish = (values) => {
+		console.log('Received values:', values);
+	};
 
-          <div className={styles.checkboxForgot}>
-            <div>
-              <input type="checkbox" id="remember" />
-              <label htmlFor="remember">Remember password?</label>
-            </div>
+	return (
+		<div className={styles.loginPageContainer}>
+			<div className={styles.leftSide}>
+				<div className={styles.loginHeader}>
+					<h1>Sign In</h1>
+				</div>
+				<div className={styles.loginForm}>
+					<Form
+						name="basic"
+						initialValues={{remember: true}}
+						onFinish={onFinish}
+						className={styles.formContainer}
+					>
+						<Form.Item
+							className={styles.formItem}
+							label="Email"
+							name="email"
+							rules={[{required: true, message: 'Please input your email!'}]}
+						>
+							<Input className={`${styles.inputField} ${styles.inputField1}`} />
+						</Form.Item>
 
-            <a href="/signup" className={styles.forgotPassword}>
-              Forgot password?
-            </a>
-          </div>
-          <button className={styles.loginButton}>Sign In</button>
-        </div>
-      </div>
-      <div
-        className={styles.rightSide}
-      ><img
-						style={{
-							width: '100%',
-							maxHeight: '100%',
-							objectPosition: 'center',
-							objectFit: 'cover',
-						}}
-						src={imageExporter.background}
-						alt="background"
-					></img></div>
-    </div>
-  );
+						<Form.Item
+							className={styles.formItem}
+							label="Password"
+							name="password"
+							rules={[{required: true, message: 'Please input your password!'}]}
+						>
+							<Input.Password className={styles.inputField} />
+						</Form.Item>
+
+						<div className={styles.formItemsContainer}>
+							<Form.Item name="remember" valuePropName="checked">
+								<Checkbox className={styles.rememberCheckbox}>
+									Remember password?
+								</Checkbox>
+							</Form.Item>
+
+							<Form.Item>
+								<a href="/signup" className={styles.forgotPassword}>
+									Forgot password?
+								</a>
+							</Form.Item>
+						</div>
+
+						<Form.Item>
+							<Button type="primary" htmlType="submit" className={styles.loginButton}>
+								Sign In
+							</Button>
+						</Form.Item>
+					</Form>
+				</div>
+			</div>
+			<div className={styles.rightSide}>
+				<img
+					style={{
+						width: '100%',
+						maxHeight: '100%',
+						objectPosition: 'center',
+						objectFit: 'cover',
+					}}
+					src={imageExporter.background}
+					alt="background"
+				></img>
+			</div>
+		</div>
+	);
 };
 
 export default LoginPage;
