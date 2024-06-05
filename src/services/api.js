aa4e1167dd635091782f118c066866a81176cd59
votantai3070 <votantai.07092002@gmail.com>
2024-06-05 18:23:04 +0700

    FEAT: Axios response interceptor to handle token expiration and renewal

--- src/services/api.js
+++ src/services/api.js
@@ -24,20 +24,20 @@ export const api = axios.create({
 });
 
 // Axios response interceptor to handle token expiration and renewal
-// api.interceptors.response.use(
-// 	(response) => {
-// 		return response;
-// 	},
-// 	async (error) => {
-// 		if (error.response.status === 401) {
-// 			console.log('401 error');
-// 			window.location.href = '/login';
-// 		}
-// 		if (error.response.status === 403) {
-// 			console.log('403 error');
-// 			window.location.href = '/permission-denied';
-// 			toast.error('403 error');
-// 		}
-// 		return Promise.reject(error);
-// 	}
-// );
+api.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	async (error) => {
+		if (error.response.status === 401) {
+			console.log('401 error');
+			window.location.href = '/login';
+		}
+		if (error.response.status === 403) {
+			console.log('403 error');
+			window.location.href = '/permission-denied';
+			toast.error('403 error');
+		}
+		return Promise.reject(error);
+	}
+);
